80e27a86414da1dfa17e2d692ec5a8aad080e2a1
andy <andy@ulendo.com>
2017-03-27 23:01:37 +0200

    updates

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,7 +1,9 @@
 class CarsController < ApplicationController
 
   def index
-    @makes=Make.all.order('RANDOM()')
+    #@makes=Make.all.order('RANDOM()')
+    @makes=Make.find_by_sql('SELECT m.id,m.name,count(*) as cnt FROM makes m inner join cars c on c.make_id=m.id inner join entries e on e.car_id=c.id group by m.id,m.name order by cnt desc')
+
   end
 
   def show
