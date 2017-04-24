c2525b4b51c50d46b51a375484103d6f97a1d2f4
andy <andy@ulendo.com>
2017-04-24 10:52:26 +0200

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
 
   #photos
   get 'photo/:id', to: 'photos#show'
-  get 'photos/views', to: 'photos#views'
+  get 'admin/photo_views', to: 'photos#views'
 
   #JSON STUFF
   get ':id/guards',constraints: {id: /\d{4}/}, to: 'guards#json_index'
