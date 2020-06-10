7534178ba736d99ff747fc9dda2d09816ce2294e
Andy Fleminf <andy@ulendo.com>
2020-06-10 14:26:57 +0200

    2020 updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    @sponsors=Sponsor.all.where('logo_file_name IS NOT NULL')
+    #@sponsors=Sponsor.all.where('logo_file_name IS NOT NULL')
     @charges=Charge.past
     @teams=Team.all
     @raised=@charges.reduce(0) {|a,b| a+b.raised_dollars }
@@ -12,12 +12,23 @@ class ApplicationController < ActionController::Base
     @current_entries=Entry.current
     @current_charge=Charge.current.first
     @previous_charge=Charge.past.first
-    @current_cpsponsors=Sponsor.current
-    @current_sponsors_nm=ChargeSponsor.current.naming
-    @current_sponsors_mj=ChargeSponsor.current.major
-    @current_sponsors_st=ChargeSponsor.current.standard
+    if (@current_charge.charge_sponsors.major.count + @current_charge.charge_sponsors.standard.count + @current_charge.sponsors.count) > 0
+      @sponsors_charge=@current_charge
+    else
+      @sponsors_charge=@previous_charge
+    end
+
+    #@current_cpsponsors=Sponsor.current
+    #@current_sponsors_nm=ChargeSponsor.current.naming
+    #@current_sponsors_mj=ChargeSponsor.current.major
+    #@current_sponsors_st=ChargeSponsor.current.standard
     #@current_beneficiaries=Charge.past.order(:charge_date).last.grants.order("RANDOM()")
-    @current_beneficiaries=Charge.find_by_ref('2018').grants.order("RANDOM()")
+    if @current_charge.grants.count>0
+
+      @current_beneficiaries=@current_charge.grants #Charge.find_by_ref('2018').grants.order("RANDOM()")
+    else
+      @current_beneficiaries=@previous_charge.grants
+    end
   end
 
   def about
